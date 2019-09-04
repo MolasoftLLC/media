c20b88bb1a9d04feb3d77fb329a3b41a13693c88
mukae9 <rockjoint2016@gmail.com>
2019-09-04 12:31:31 +0900

    API通信成功

---
+++ media/app/Http/Controllers/LanceController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Lancer;
+use SebastianBergmann\Environment\Console;
+
+class LanceController extends Controller
+{
+    public function index()
+    {
+        $lancer = Lancer::all()->take(3);
+        return $lancer;
+    }
+
+    public function lancer($id)
+    {
+        // $lancer = Lancer::all()->take(1);
+        $lancer = Lancer::where('id',$id)->get();
+        return $lancer;
+    }
+
+}
---
+++ media/database/migrations/2019_09_03_005038_free_lancers.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FreeLancers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lancers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->string('title');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lancers');
+    }
+}
